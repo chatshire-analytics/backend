edc792aad01416a1f282809a51d14a456111e5a9
Sigrid <sigrid.jinhyung@gmail.com>
2023-01-18 16:36:59 +0900

    feat: add HttpRequestLogHandler for every each http request

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -2,10 +2,36 @@ package api
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
 	"mentat-backend/internal/api/telegram"
+	"time"
 )
 
+func HttpRequestLogHandler(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		req := c.Request()
+		res := c.Response()
+		start := time.Now()
+		if err := next(c); err != nil {
+			c.Error(err)
+		}
+		stop := time.Now()
+		logrus.WithFields(logrus.Fields{
+			"component": "api",
+			"timestamp": time.Now().Format(time.RFC3339),
+			"method":    req.Method,
+			"remote":    req.RemoteAddr,
+			"path":      req.URL.Path,
+			"proto":     req.Proto,
+			"status":    res.Status,
+			"duration":  stop.Sub(start),
+		}).Log(logrus.InfoLevel, "request completed")
+		return nil
+	}
+}
+
 func SetupRoutes(e *echo.Echo) {
+	e.Use(HttpRequestLogHandler)
 	e.GET("/health", HealthCheck)
 	e.POST("/telegram/webhook", telegram.WebhookHandler)
 }
