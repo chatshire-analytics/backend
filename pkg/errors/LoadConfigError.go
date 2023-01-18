ddbdd15cefb6ed5b5ddba90266bc11fa527f0ab3
Sigrid <sigrid.jinhyung@gmail.com>
2023-01-18 16:36:40 +0900

    feat: add some custom errors

--- pkg/errors/LoadConfigError.go
+++ pkg/errors/LoadConfigError.go
@@ -1,42 +1,9 @@
 package setup
 
-import (
-	"github.com/labstack/echo/v4"
-	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
-	"mentat-backend/cmd/setup"
-	"mentat-backend/internal/api"
-	"mentat-backend/internal/config"
-	"os"
-)
+import "github.com/pkg/errors"
 
-func InitializeEcho() (error, *echo.Echo) {
-	e := echo.New()
-	api.SetupRoutes(e)
+const LoadConfigErrorString = "failed to load config: environment string is empty"
 
-	env := os.Getenv("ENV")
-	logrus.WithFields(logrus.Fields{
-		"component": "setup",
-		"env":       env,
-	}).Log(logrus.InfoLevel, "ENV is set to "+env)
-
-	cfg, err := config.LoadConfig(config.DefaultConfigPath, env)
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"component": "setup",
-			"env":       env,
-		}).WithError(err).Error(logrus.ErrorLevel, "Failed to load config")
-		return err, nil
-	}
-	if cfg.Environment == "" {
-		logrus.WithFields(logrus.Fields{
-			"component": "setup",
-			"env":       env,
-		}).WithError(err).Error(logrus.ErrorLevel, "Failed to load config")
-		return errors.New("failed to load config: environment string is empty"), nil
-	}
-
-	setup.ConfigHandler(e, cfg)
-
-	return nil, e
+func LoadConfigError() error {
+	return errors.New(LoadConfigErrorString)
 }
--- pkg/errors/TelegramError.go
+++ pkg/errors/TelegramError.go
@@ -1,1 +1,9 @@
 package setup
+
+import "github.com/pkg/errors"
+
+const TelegramErrorString = "unexpecte Http Status Code from Telegram API"
+
+func TelegramError(httpStatus string) error {
+	return errors.New(TelegramErrorString + " httpStatus: " + httpStatus)
+}
