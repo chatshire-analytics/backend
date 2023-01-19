595e430b898b50e4244e4210e1bb65380335e0bc
Sigrid <sigrid.jinhyung@gmail.com>
2023-01-19 17:14:03 +0900

    chore: temporarily add main.go

--- chatgpt-service/cmd/main.go
+++ chatgpt-service/cmd/main.go
@@ -1,1 +1,18 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+)
+
+var k = koanf.New(".")
+
+func main() {
+	err := k.Load(file.Provider("config.yaml"), yaml.Parser())
+	if err != nil {
+		return
+	}
+	fmt.Println(k.String("dev.OpenAI.API_KEY"))
+}
