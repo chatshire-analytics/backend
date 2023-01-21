8d25ddac63454a0bda45b9392c8f6775b64b7a62
Sigrid <sigrid.jinhyung@gmail.com>
2023-01-21 20:14:34 +0900

    refactor: ModelIdParamKey to have model_id

--- chatgpt-service/pkg/client/model.go
+++ chatgpt-service/pkg/client/model.go
@@ -86,6 +86,7 @@ curl https://api.openai.com/v1/models \
 const ModelEndPoint = "/models"
 const GetAllModels = ModelEndPoint + "/all"
 const RetrieveModels = ModelEndPoint + "/retrieve"
+const ModelIdParamKey = "model_id"
 
 type PermissionInModelObject struct {
 	ID                 string      `json:"id"`
