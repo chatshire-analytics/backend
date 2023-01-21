cd37020480d46d7244d6eb32e80753d5a29d5056
Sigrid <sigrid.jinhyung@gmail.com>
2023-01-21 19:29:52 +0900

    test: add TestCreateCompletionStreamTrue method

--- chatgpt-service/test/openai_client_test.go
+++ chatgpt-service/test/openai_client_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"bufio"
 	"bytes"
 	"chatgpt-service/cmd/setup"
 	"chatgpt-service/internal/api"
@@ -9,10 +10,13 @@ import (
 	"chatgpt-service/internal/pkg/engine"
 	"chatgpt-service/pkg/client"
 	"encoding/json"
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -33,6 +37,32 @@ func setupTest(t *testing.T, method string, endpoint string, bodyRaw *[]byte) (e
 		body = bytes.NewBuffer(*bodyRaw)
 	}
 	reqRaw := httptest.NewRequest(method, endpoint, body)
+	reqRaw.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	req := e.NewContext(reqRaw, httptest.NewRecorder())
+	req.Set(cpkg.OpenAIClientKey, oc)
+	return nil, req
+}
+
+func setupTestSSE(t *testing.T, method string, endpoint string, bodyRaw *[]byte) (error, echo.Context) {
+	cfg, err := config.LoadConfig(config.TestConfigPath, "dev")
+	if err != nil {
+		t.Errorf("could not load config: %v", err)
+	}
+	oc, err := setup.NewOpenAIClient(cfg)
+	if err != nil {
+		t.Errorf("could not create openai client: %v", err)
+	}
+	e := echo.New()
+	var body io.Reader
+	if bodyRaw == nil {
+		body = nil
+	} else {
+		body = bytes.NewBuffer(*bodyRaw)
+	}
+	reqRaw := httptest.NewRequest(method, endpoint, body)
+	reqRaw.Header.Set(echo.HeaderCacheControl, "no-cache")
+	reqRaw.Header.Set(echo.HeaderAccept, "text/event-stream")
+	reqRaw.Header.Set(echo.HeaderConnection, "keep-alive")
 	reqRaw.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	req := e.NewContext(reqRaw, httptest.NewRecorder())
 	req.Set(cpkg.OpenAIClientKey, oc)
@@ -95,7 +125,7 @@ func TestRetrieveModel(t *testing.T) {
 
 func TestCreateCompletion(t *testing.T) {
 	// given
-	bodyTest := client.NewCompletionRequest("this is a test", 3, nil)
+	bodyTest := client.NewCompletionRequest("this is a test", 3, nil, nil)
 	bodyRaw, err := json.Marshal(bodyTest)
 	if err != nil {
 		t.Errorf("could not marshal request body: %v", err)
@@ -124,4 +154,47 @@ func TestCreateCompletion(t *testing.T) {
 	if len(completionResponse.Choices) == 0 {
 		t.Errorf("expected at least one completion but got %v", len(completionResponse.Choices))
 	}
+}
+
+func TestCreateCompletionStreamTrue(t *testing.T) {
+	// given
+	stream := new(bool)
+	*stream = true
+	bodyTest := client.NewCompletionRequest("one thing that you should know about golang", 20, nil, stream)
+	bodyRaw, err := json.Marshal(bodyTest)
+	if err != nil {
+		t.Errorf("could not marshal request body: %v", err)
+	}
+	err, req := setupTestSSE(t, http.MethodPost, client.CreateCompletionEndpoint, &bodyRaw)
+	if err != nil {
+		t.Errorf("could not setup test: %v", err)
+	}
+
+	// when
+	err = api.CreateCompletionStream(req)
+
+	// then
+	if err != nil {
+		t.Errorf("could not create completion: %v", err)
+	}
+	res := req.Response()
+	if res.Status != http.StatusOK {
+		t.Errorf("expected status OK but got %v", res.Status)
+	}
+	if res.Header().Get(echo.HeaderContentType) != "text/event-stream" {
+		t.Errorf("expected content type text/event-stream but got %v", res.Header().Get(echo.HeaderContentType))
+	}
+	var rawString string
+	reader := bufio.NewReader(res.Writer.(*httptest.ResponseRecorder).Body)
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil {
+			if strings.HasSuffix(string(line), "\\end ") {
+				break
+			}
+		}
+		rawString += string(line)
+	}
+	fmt.Println("rawString", rawString)
+	assert.NotEmpty(t, rawString)
 }
